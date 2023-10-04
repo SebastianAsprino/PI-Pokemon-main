ed46465a4c4c6470924aea5617727be52ee81376
Sebastian Asprino <asprinosebastian@proton.me>
2023-10-03 21:45:16 -0500

    redit 2

--- client/src/components/card/card.jsx
+++ client/src/components/card/card.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
-import styles from '../Card/Card.module.css'
+// import styles from '../Card/Card.module.css'
 
 function Card({id, name, imagen,type}) {
   return (
-    <div className={styles.individual_card}>        
-            <h5 className={styles.name} >{name}</h5>                
-            <img className={styles.img} src={imagen}/>
-            <h5 className={styles.type}>{type}</h5>     
-            <Link to={`/pokemons/${id}`} className={styles.detail}>
+    <div >        
+            <h5  >{name}</h5>                
+            <img  src={imagen}/>
+            <h5 >{type}</h5>     
+            <Link to={`/pokemons/${id}`} >
                 DETAILS
             </Link>
     </div>
