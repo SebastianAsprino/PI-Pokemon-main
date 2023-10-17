c485fd881070eb378bde2ee58259f09a220770da
Sebastian Asprino <asprinosebastian@proton.me>
2023-10-16 20:43:24 -0500

    error con el nombre del archivo

--- client/src/components/card/card.jsx
+++ client/src/components/card/card.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
-import './Card.css'
+import './card.css'
 
 function Card({id, name, imagen, hp, attack, speed, type}) {
   return (
