ce01d25da28a475dcc3b85757609f713755aff1c
Sebastian Asprino <asprinosebastian@proton.me>
2023-11-19 20:06:28 -0500

    creacion del pie de pagina

---
+++ client/src/components/footer/footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './footer.css'; // Crea un archivo de estilo para el footer si aún no lo tienes
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>&copy; 2023 Proyecto Individual SoyHenry - Hecho por <strong>Sebastian Asprino</strong></p>
+    </footer>
+  );
+}
+
+export default Footer;
