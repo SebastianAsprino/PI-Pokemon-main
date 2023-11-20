a3c7cb58aa844e4d3a0362e61581d49512effaca
Sebastian Asprino <asprinosebastian@proton.me>
2023-11-19 19:28:21 -0500

    correcion de class a classname

--- client/src/components/loading/loading.jsx
+++ client/src/components/loading/loading.jsx
@@ -2,12 +2,12 @@ import './loading.css'
 
 function Loading() {
   return (
-  <div class="pl">
-	<div class="pl__outer-ring"></div>
-	<div class="pl__inner-ring"></div>
-	<div class="pl__ball">
-    <div class="pl__ball-texture"></div>
-    <div class="pl__ball-inner-shadow"></div>
+  <div className="pl">
+	<div className="pl__outer-ring"></div>
+	<div className="pl__inner-ring"></div>
+	<div className="pl__ball">
+    <div className="pl__ball-texture"></div>
+    <div className="pl__ball-inner-shadow"></div>
 	</div>
   </div>
   )
