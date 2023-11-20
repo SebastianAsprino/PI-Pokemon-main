7d77dec2a87bd51b1d3d9f379ba38264419199e0
Sebastian Asprino <asprinosebastian@proton.me>
2023-11-19 20:06:52 -0500

    agregacion del pie de pagina

--- client/src/components/home/home.jsx
+++ client/src/components/home/home.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import NavBar from '../../components/navbar/NavBar'
 import Cards from '../../components/cards/cards'
+import Footer from '../../components/footer/footer'
 
 function Home() {
   return (
     <div>
       <NavBar/>
-      <div>
+      <section>
         <Cards/>
-      </div>
+      </section>
+      <Footer/>
     </div>
   )
 }
